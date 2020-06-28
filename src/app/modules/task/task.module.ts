9db56706bc78881ab80eb5c9e2d83e113dc3c910
laurentiu-develop <dobresculaurentiu97@gmail.com>
2020-06-28 20:20:10 +0300

    -deploy refactor

--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -6,6 +6,8 @@ import { TaskNewComponent } from './components/task-new/task-new.component';
 import { TaskFormComponent } from './forms/task-form/task-form.component';
 import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SubTaskFormComponent } from 'src/app/shared/sub-task/forms/sub-task-form/sub-task-form.component';
+import { SubTaskNewComponent } from 'src/app/shared/sub-task/components/sub-task-new/sub-task-new.component';
 
 const routes: Routes = [
   {
@@ -20,6 +22,6 @@ const routes: Routes = [
     SharedModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [TaskPage, TaskNewComponent, TaskDetailComponent, TaskFormComponent],
+  declarations: [TaskPage, TaskNewComponent, TaskDetailComponent, TaskFormComponent, SubTaskFormComponent, SubTaskNewComponent],
 })
 export class TaskPageModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:63779/'
+  url: 'http://178.128.206.203:80/'
 };
